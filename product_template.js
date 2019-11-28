8bce26a98a280e4556faac8b8a8988f0c3018240
elli raynai <elli@cinehackers.com>
2019-11-28 12:27:37 -0500

    committing

---
+++ product_template.js
@@ -0,0 +1,21 @@
+const products = [{
+    Product_image: 'Blue Cat Carrier',
+    Product_color_1: 'g',
+    Product_color_2: 'r',
+    Product_color_name_1: 'Green',
+    Product_color_name_2: 'Red',
+    Product_size_value_1: 's',
+    Product_size_value_2: 'm',
+    Product_size_value_3: 'l',
+    Qty_size_1: '30',
+    Qty_size_2: '20',
+    Qty_size_3: '14',
+    Product_size_name_1: 'Small',
+    Product_size_name_2: 'Medium',
+    Product_size_3name_3: 'Large',
+    Regular_price: '$50.00',
+    Sale_price: '$35.00',
+
+}
+
+] 
