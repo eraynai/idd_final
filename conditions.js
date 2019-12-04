cacc7e376d71e524d1d82e8ff9bb5854ad96b5aa
elli raynai <elli@cinehackers.com>
2019-12-04 14:57:53 -0500

    latest changes

---
+++ conditions.js
@@ -0,0 +1,21 @@
+let temp = 3;
+
+if (temp < 0){
+    //Runs if the condition is true
+    //if false, it ignores the statement
+
+
+}
+
+let greatdeal = '';
+
+if (course.cost < 1000){
+    greatdeal = '<small class="call out
+
+    filter by two things minimum
+
+    20 products
+    
+}
+
+return 
