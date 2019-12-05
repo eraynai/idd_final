03f4f8a8400dc89614fe886491a87dc8f788881c
elli raynai <elli@cinehackers.com>
2019-12-05 13:58:27 -0500

    Latest Commit

--- js/js_final.js
+++ js/js_final.js
@@ -1,30 +1,199 @@
 const products = [{
     Title: "Space Capsule Cat Carrier",
     Type: "Anti-Allergic",
-    Construction: "Durable",
+    Construction: "Machine Made",
     Warranty: "3 Year Warranty",
     Price: "$29.99",
     Image: "img/carrier_1.jpeg",
     Promotion: true,
     Filter: "space capsules"
 }, {
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "1 Year Warranty",
+    Price: "$29.99",
+    Image: "img/carrier_2.jpg",
+    Promotion: false,
+    Filter: "mesh bags"
+}, {
+   Title: "Space Capsule Cat Carrier",
+   Type: "Anti-Allergic",
+   Constuction: "Machine Made",
+   Warranty: "3 Year Warranty",
+   Price: "$34.99",
+   Image: "img/carrier_3.jpg",
+   Promotion: false,
+   Filter: "space capsules"
+}, {
+
+    Title: "Space Capusle Cat Carrier",
+    Type: "Anti-Allergic",
+    Construction: "Machine Made",
+    Warranty: "3 Year Warranty",
+    Price: "$29.99",
+    Image: "img/carrier_4.jpg",
+    Promotion: true,
+    Filter: "space capsules"
+}, {
+    Title: "Space Capusle Cat Carrier",
+    Type: "Anti-Allergic",
+    Construction: "Machine Made",
+    Warranty: "3 Year Warranty",
+    Price: "$29.99",
+    Image: "img/carrier_5.jpg",
+    Promotion: true,
+    Filter: "space capsules" 
+
+},{
+    Title: "Space Capusle Cat Carrier",
+    Type: "Anti-Allergic",
+    Construction: "Machine Made",
+    Warranty: "3 Year Warranty",
+    Price: "$29.99",
+    Image: "img/carrier_6.jpg",
+    Promotion: true,
+    Filter: "space capsules"
+
+},{
+    Title: "Hard Case Cat Carrier",
+    Type: "Durable",
+    Construction: "Mold Enjection",
+    Warranty: "5 Year Warranty",
+    Price: "$79.99",
+    Image: "img/carrier_7.jpg",
+    Promotion: false,
+    Filter: "hard case"
+},{
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "2 Year Warranty",
+    Price: "$39.99",
+    Image: "img/carrier_8.jpg",
+    Promotion: false,
+    Filter: "mesh bags"
+
+},{
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Leather",
+    Construction: "Hand Made",
+    Warranty: "2 Year Warranty",
+    Price: "$49.99",
+    Image: "img/carrier_9.jpg",
+    Promotion: false,
+    Filter: "mesh bags"
+
+},{
 
     Title: "Mesh Bag Cat Carrier",
     Type: "Soft",
     Construction: "Hand Made",
+    Warranty: "2 Year Warranty",
     Price: "$49.99",
     Image: "img/carrier_10.jpg",
     Promotion: false,
     Filter: "mesh bags"
-}, {
+},{
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "2 Year Warranty",
+    Price: "$19.99",
+    Image: "img/carrier_11.jpg",
+    Promotion: true,
+    Filter: "mesh bags"
+
+},{
+
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "3 Year Warranty",
+    Price: "$39.99",
+    Image: "img/carrier_12.jpg",
+    Promotion: false,
+    Filter: "mesh bags"
+
+},{
+
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "3 Year Warranty",
+    Price: "$59.99",
+    Image: "img/carrier_13.jpg",
+    Promotion: false,
+    Filter: "mesh bags"
+},{
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "3 Year Warranty",
+    Price: "$19.99",
+    Image: "img/carrier_14.jpg",
+    Promotion: true,
+    Filter: "mesh bags"
+
+},{
+
+    Title: "Space Capusle Cat Carrier",
+    Type: "Anti-Allergic",
+    Construction: "Machine Made",
+    Warranty: "3 Year Warranty",
+    Price: "$24.99",
+    Image: "img/carrier_15.jpeg",
+    Promotion: true,
+    Filter: "space capsules"
+
+},{
+    Title: "Space Capusle Cat Carrier",
+    Type: "Anti-Allergic",
+    Construction: "Machine Made",
+    Warranty: "3 Year Warranty",
+    Price: "$34.99",
+    Image: "img/carrier_16.jpg",
+    Promotion: false,
+    Filter: "space capsules"
+
+},{
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "3 Year Warranty",
+    Price: "$19.99",
+    Image: "img/carrier_17.jpg",
+    Promotion: true,
+    Filter: "mesh bags"
+},{
+
+    Title: "Mesh Bag Cat Carrier",
+    Type: "Soft",
+    Construction: "Hand Made",
+    Warranty: "3 Year Warranty",
+    Price: "$29.99",
+    Image: "img/carrier_18.jpg",
+    Promotion: true,
+    Filter: "mesh bags"
+
+},{
 
     Title: "Hard Case Cat Carrier",
     Type: "Durable",
     Constuction: "Hard Plastic with Metal Parts",
-    Warranty: "5 Years",
+    Warranty: "5 Year Warranty",
     Price: "$79.99",
     Image: "img/carrier_19.jpg",
     Promotion: false,
+    Filter: "hard case"
+},{
+    Title: "Hard Case Cat Carrier",
+    Type: "Durable",
+    Constuction: "Hard Plastic with Metal Parts",
+    Warranty: "5 Year Warranty",
+    Price: "$59.99",
+    Image: "img/carrier_20.jpg",
+    Promotion: true,
     Filter: "hard case"
 }
 ];
